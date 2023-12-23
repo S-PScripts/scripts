f0b2d9052662854543d8f83f0945dbd01331fbb0
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-12-23 18:08:28 +0000

    Update Crash Scripts.lua

--- Luau/KAH/Other Scripts/Crash Scripts.lua
+++ Luau/KAH/Other Scripts/Crash Scripts.lua
@@ -4,11 +4,15 @@ for i = 1,100 do
     game.Players:Chat("clone all all all")
 end
 
+game.Players:Chat("reload me")
+
 print("trying freeze crash...")
 for i = 1,100 do
     game.Players:Chat("freeze all all all")
     game.Players:Chat("clone all all all")
 end
+
+game.Players:Chat("reload me")
 
 print("trying rocket/shield crash...")
 for i = 1,100 do
@@ -16,6 +20,8 @@ for i = 1,100 do
     game.Players:Chat("rocket/all/all/all")
     game.Players:Chat("clone/all/all/all")
 end
+
+game.Players:Chat("reload me")
 
 print("trying vg crash...")
 game.Players:Chat("gear me 0000000000000000000000000094794847")
@@ -29,5 +35,7 @@ wait(.15)
 for i = 1,100 do
     game.Players:Chat("unsize me me me")
 end
+
+game.Players:Chat("reload me")
 
 print("the server should be crashed! sorry if it didn't.")
