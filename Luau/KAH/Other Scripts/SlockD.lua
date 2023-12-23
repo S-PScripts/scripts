754b18cab60aae866a3da251ec1c109766ddc2c8
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-12-23 18:28:09 +0000

    Update SlockD.lua

--- Luau/KAH/Other Scripts/SlockD.lua
+++ Luau/KAH/Other Scripts/SlockD.lua
@@ -1,4 +1,4 @@
-local prefix = '!'
+local prefix = '-'
 local whitelist = {"scriptingprogrammer","me_123eq","me_crashking"}
 local blacklist = {}
 local slockenabled = false
@@ -8,15 +8,29 @@ function checkforplayer(plr)
   for i, v in pairs(game.Players:GetPlayers()) do
       if string.sub(v.Name:lower(), 1, #plr) == plr then
           player = v.Name
-          print(player)
+          print(player,"was found.")
       end
   end
 end
 
 game.Players:Chat("h \n\n\n [SlockD] Please be careful, this server is protected. \n\n\n")
+print("-Commands-")
+print("-slock/-unslock -> Serverlock the whole server.")
+print("-wl/-unwl -> Let a user bypass the serverlock.")
+print("-bl/unbl -> Serverlock a particular individual.")
+
+print("-dcrash -> dog crash the server")
+print("-fcrash -> freeze crash the server")
+print("-mcrash -> messagee crash the server")
+print("-rcrash -> rocket crash the server")
+print("-scrash -> shield crash the server")
+print("-vcrash -> vamp crash the server")
+
+print("-mkick -> message kick a player")
+print("-rkick -> rocket kick a player")
+print("SlockD by S_P. The simple base for KohlsLite.")
 
 -- this is similar to CMD's system :D
-
 task.spawn(function()
     while true do
         local players = game.Players:GetPlayers()
@@ -106,4 +120,72 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         game.Players:Chat('unblind all')
         game.Players:Chat('unpunish all')
     end
+
+    if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'dcrash' then
+        print("trying dog crash...")
+        for i = 1,100 do
+            game.Players:Chat("dog all all all")
+            game.Players:Chat("clone all all all")
+        end
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'fcrash' then
+        print("trying freeze crash...")
+        for i = 1,100 do
+            game.Players:Chat("freeze all all all")
+            game.Players:Chat("clone all all all")
+        end
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'rcrash' then
+        print("trying rocket crash...")
+        for i = 1,100 do
+            game.Players:Chat("rocket/all/all/all")
+            game.Players:Chat("clone/all/all/all")
+        end
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'mkick' then
+        	local acplr = string.sub(msg:lower(), #prefix + 7)
+          for i = 1,100 do
+              game.Players:Chat("pm/"..acplr.." [KohlsLite]: 😀😃😄😁😆😅😂🤣😭💀💀💀💀💀💀💀💀💀😀😃😄😁😆😅😂🤣😭💀💀💀💀💀💀💀💀💀")
+          end
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'rkick' then
+        	local rcplr = string.sub(msg:lower(), #prefix + 7)
+          for i = 1,100 do
+              game.Players:Chat("rocket/"..rcplr.."/"..rcplr.."/"..rcplr)
+          end
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'mcrash' then
+          for i = 1,100 do
+              game.Players:Chat("pm/all [KohlsLite]: 😀😃😄😁😆😅😂🤣😭💀💀💀💀💀💀💀💀💀😀😃😄😁😆😅😂🤣😭💀💀💀💀💀💀💀💀💀")
+          end
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'scrash' then
+        print("trying shield crash...")
+        for i = 1,100 do
+            game.Players:Chat("shield/all/all/all")
+            game.Players:Chat("clone/all/all/all")
+        end
+    end
+    
+    if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'vcrash' then
+        print("trying vg crash...")
+        game.Players:Chat("gear me 0000000000000000000000000094794847")
+        local Backpack = game.Players.LocalPlayer:FindFirstChildOfClass("Backpack")
+        game.Players.LocalPlayer.Backpack:WaitForChild("VampireVanquisher")
+        for _, v in ipairs(Backpack:GetChildren()) do
+                v.Parent = game.Players.LocalPlayer.Character
+                v:Activate()
+        end
+        wait(.15)
+        for i = 1,100 do
+            game.Players:Chat("unsize me me me")
+        end
+    end
+    
 end)          
