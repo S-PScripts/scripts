04c471cc3b4218cef1e825c4aab4aeb91cd909c7
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-01-23 20:26:12 +0000

    Update and rename KL Loadstring.lua to Script Loadstring.lua

---
+++ Luau/KAH/Main Scripts/Script Loadstring.lua
@@ -0,0 +1,6 @@
+-- KohlsLite
+loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/KohlsLite.lua"))()
+
+-- CrashX
+loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/Crash%20X.lua"))()
+
