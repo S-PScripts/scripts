2e28b8e3bd870963694a5235530513fbdd406ab9
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-01-27 16:56:44 +0000

    Update Script Loadstring.lua

--- Luau/KAH/Main Scripts/Script Loadstring.lua
+++ Luau/KAH/Main Scripts/Script Loadstring.lua
@@ -1,6 +1,7 @@
--- KohlsLite
-loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/KohlsLite.lua"))()
-
 -- CrashX
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/Crash%20X.lua"))()
+task.wait(5)
+
+-- KohlsLite
+loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/KohlsLite.lua"))()
 
