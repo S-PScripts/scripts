e813d2f64cc226a9f8daac8e25311ba2a59111e2
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-01-27 17:09:07 +0000

    Update Crash X.lua

--- Luau/KAH/Main Scripts/Crash X.lua
+++ Luau/KAH/Main Scripts/Crash X.lua
@@ -20,26 +20,3 @@ local function Chat(msg)
 end
 
 -- // get a pad \\ --
-local function GetPad(msg)
-            while PadCheck == true do
-                task.wait(0)
-                if not game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild(game.Players.LocalPlayer.Name .. "'s admin") then
-                    if game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin") then
-                        local pad = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head")
-                        local padCFrame = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head").CFrame
-                        task.wait(0.125)
-                        pad.CanCollide = false
-                        repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-                        pad.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-                        task.wait(0.125)
-                        pad.CFrame = padCFrame
-                        pad.CanCollide = true
-                    else
-                        fireclickdetector(game:GetService("Workspace").Terrain["_Game"].Admin.Regen.ClickDetector, 0)
-                    end
-                end
-            end
-end
-
-PadCheck = true
-GetPad()
