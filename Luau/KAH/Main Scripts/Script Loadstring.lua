a590ee19654c4d810c5764ddc59039438b65b495
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-01-27 17:07:11 +0000

    Update Script Loadstring.lua

--- Luau/KAH/Main Scripts/Script Loadstring.lua
+++ Luau/KAH/Main Scripts/Script Loadstring.lua
@@ -3,13 +3,15 @@ execu = false
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/Crash%20X.lua"))()
 task.wait(5)
 execu = true
-
+print("should execute")
 -- KohlsLite
 task.spawn(function()
     while true do
         task.wait(0)
         if execu == true then
+            print("executed?")
             loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/KohlsLite.lua"))()
+            break
         end
     end
 end)
