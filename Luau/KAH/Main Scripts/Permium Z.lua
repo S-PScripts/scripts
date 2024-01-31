172035ae5ef6010201428b072d2e1afc17f4d374
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-01-31 20:23:11 +0000

    Update Permium Z.lua

--- Luau/KAH/Main Scripts/Permium Z.lua
+++ Luau/KAH/Main Scripts/Permium Z.lua
@@ -1,13 +1,27 @@
--- Permium Z unfinished
+-- Permium Z
+
 print("Permium Z booted. Created by S_P.")
 
 -- variables
-perm = false
-perm2 = true
-loopgrab = false
+local prefix = "!"
 
-padreinforcements = true
-padbanned = {"sgoslee"}
+local perm = false
+local perm2 = true
+local loopgrab = false
+
+local padreinforcements = true
+local padbanned = {"sgoslee"}
+
+-- player finder
+function PLAYERCHECK(plr)
+  for i, v in pairs(game.Players:GetPlayers()) do
+      if string.sub(v.Name:lower(), 1, #plr) == plr then
+          player = v.Name
+	  cplr = v
+          print("Found"..player)
+      end
+  end
+end
 
 print("-commands-")
 print("perm/unperm - perm pad")
@@ -15,6 +29,72 @@ print("perm2/unperm2 - perm 2 pad")
 print("lg/unlg - loopgrab pads")
 print("padreinf/unpadreinf - pad reinforcements")
 print("padban/unpadban - pad ban user")
+print("regen - regen the pad")
+
+-- Chat
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+		
+	if string.sub(msg:lower(), 1, #prefix + 2) == prefix.."lg" then
+	         loopgrab = true
+        end
+		
+	if string.sub(msg:lower(), 1, #prefix + 4) == prefix.."unlg" then
+	         loopgrab = false
+        end
+
+	if string.sub(msg:lower(), 1, #prefix + 4) == prefix.."perm" then
+	         perm = true
+        end
+		
+	if string.sub(msg:lower(), 1, #prefix + 6) == prefix.."unperm" then
+	         perm = false
+        end
+		
+	if string.sub(msg:lower(), 1, #prefix + 5) == prefix.."perm2" then
+	         perm2 = true
+        end
+		
+	if string.sub(msg:lower(), 1, #prefix + 7) == prefix.."unperm2" then
+	         perm2 = false
+        end
+		
+	if string.sub(msg:lower(), 1, #prefix + 6) == prefix.."padban" then
+	         local dasplayer = string.sub(msg:lower(), #prefix + 8)
+         	 PLAYERCHECK(dasplayer)
+         	 if player ~= nil then
+                	Chat("h \n\n\n [PermiumZ]: "..player.." has been padbanned. \n\n\n")
+                	table.insert(padbanned, player)
+        	 else
+                	print('Cannot find player with the name: '..dasplayer)
+         	end
+        end
+		
+	if string.sub(msg:lower(), 1, #prefix + 8) == prefix.."unpadban" then
+	         local dasplayer = string.sub(msg:lower(), #prefix + 10)
+         	 PLAYERCHECK(dasplayer)
+         	 if player ~= nil then
+                	Chat("h \n\n\n [PermiumZ]: "..player.." has been padbanned. \n\n\n")
+                	table.remove(padbanned, table.find(padbanned, player))
+        	 else
+                	print('Cannot find player with the name: '..dasplayer)
+         	end
+        end
+
+	if string.sub(msg, 1, #prefix + 8) == prefix..'padreinf' then
+       		Chat("h \n\n\n [Permium Z]: Pad reinforcements are on. \n\n\n")
+      		padreinforcements = true
+    	end
+
+    	if string.sub(msg, 1, #prefix + 10) == prefix..'unpadreinf' then
+       		Chat("h \n\n\n [Permium Z]: Pad reinforcements are off! \n\n\n")
+       		padreinforcements = false
+    	end
+
+	if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'regen' then
+		Regen()
+    	end
+		
+end)
 
 -- Perm
 task.spawn(function()
