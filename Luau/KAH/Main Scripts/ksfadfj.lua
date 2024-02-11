f6cca2dd55a8ce1af7024fc90e2156fcb0984822
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-02-11 12:59:26 +0000

    Create ksfadfj.lua

---
+++ Luau/KAH/Main Scripts/ksfadfj.lua
@@ -0,0 +1,14 @@
+local musiclist = 
+{"9048375035", -- 1
+"6680495507", -- 2
+"6529070845", -- 3
+"6917155909", -- 4
+"6913550990", -- 5
+"6847929757"} -- 6
+      
+local musicnames = {"All dropping 8 beats", -- 1
+	"Robbery", -- 2
+	            "Loud music", -- 3
+	            "They trying to be cray", -- 4
+	            "TLT FNAF 2", -- 5
+                    "LONER", -- 6
