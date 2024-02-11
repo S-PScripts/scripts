9b0d9f7bd84ab736d1bb0f4ebf1a8de633a63392
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-02-11 12:58:29 +0000

    Update KohlsPaint.lua

--- Luau/KAH/Main Scripts/KohlsPaint.lua
+++ Luau/KAH/Main Scripts/KohlsPaint.lua
@@ -1,1 +1,32 @@
+themelist = {"sunshine","blossom"}
+themecode = {
+    [[fogend 0, time 12]],
+    [[fogend 100, time 0]]
+}
 
+local function Chat(msg)
+    game.Players:Chat(msg)
+end
+
+local prefix = "!"
+Chat("h KohlsPaint")
+
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    task.wait(0)
+    if string.sub(msg:lower(), 1, #prefix + 5) == prefix.."theme" then
+        local theme = string.sub(msg:lower(), #prefix + 7)
+        for i, t in ipairs(themelist) do
+            if t == theme then
+                local themeCode = themecode[i]
+                local codes = {}
+                for code in themeCode:gmatch("[^%s]+") do
+                    table.insert(codes, code)
+                end
+                for _, code in ipairs(codes) do
+                    Chat(code)
+                end
+                break
+            end
+        end
+    end
+end)
