6c372cacc31d0aa242afa2546d72a097314df377
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-02-19 09:58:15 +0000

    Delete Luau/KAH/Main Scripts/CMD POOP.lua

--- Luau/KAH/Main Scripts/CMD POOP.lua
+++
@@ -1,4 +0,0 @@
--- // cock \\ --
-
--- totally not a feature for kohlslite ;)
-game.Players:ReportAbuse(game:GetService("Players"),player.Name,"Swearing", "They kept spamming! >:( " .. math.random(1, 3276700))
