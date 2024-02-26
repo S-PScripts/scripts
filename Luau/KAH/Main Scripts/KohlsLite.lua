1cfc0ce536b1b1b75f8f1f6a019c520e3039744f
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-02-26 12:52:39 +0000

    REAL BIG UPDATE HERE
    Added an experimental fixbp for the attachment update check

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -1263,6 +1263,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
     if string.sub(msg:lower(), 1, #prefix + 3) == prefix..'bok' then
 	  bokme = (string.sub(msg:lower(), #prefix + 5))
 	  SuperCMD("dog "..bokme)
+	  task.wait(2)
+	  Chat("explode "..bokme)
     end
 	
     if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'goto2' then
@@ -1276,6 +1278,7 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
          end
     end
 
+-- OLD --
     if string.sub(msg:lower(), 1, #prefix + 9) == prefix..'moveregen' then
 	MoveRegen("move")
     end
@@ -1300,14 +1303,6 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
     if string.sub(msg:lower(), 1, #prefix + 8) == prefix..'moveobox' then
 	MoveObbyBox()
     end
-
-    if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'lrobb' then
-	 LocalObby("remove")
-    end
-
-    if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'laobb' then
-	 LocalObby("add")
-    end
 		
     if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'movebb' then
 	MoveBuildingBricks()
@@ -1324,10 +1319,6 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
     if string.sub(msg:lower(), 1, #prefix + 6) == prefix..'ufixbp' then
 	MoveBasePlate("ufix")
     end
-
-    if string.sub(msg:lower(), 1, #prefix + 7) == prefix..'toregen' then
-			regentp()
-    end
 		
     if string.sub(msg:lower(), 1, #prefix + 8) == prefix..'fixregen' then
 	MoveRegen("fix")
@@ -1337,6 +1328,32 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
 	MoveAdminPads("fix")
     end
 
+-- NEW --
+    if string.sub(msg:lower(), 1, #prefix + 7) == prefix..'exfixbp' then
+		if movestatus == true then 
+			return 
+		end
+		DisCol()
+		moveobject(Workspace_Folder.Baseplate, 2)
+		repeat fwait() until movestatus == false
+		GravFix()
+		Chat("respawn me")
+		ColFix()
+    end
+		
+-- MOVING ON... --
+    if string.sub(msg:lower(), 1, #prefix + 7) == prefix..'toregen' then
+			regentp()
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'lrobb' then
+	 LocalObby("remove")
+    end
+
+    if string.sub(msg:lower(), 1, #prefix + 5) == prefix..'laobb' then
+	 LocalObby("add")
+    end
+		
     if string.sub(msg:lower(), 1, #prefix + 9) == prefix..'findregen' then
 	regenfind = true
     end
@@ -3998,6 +4015,14 @@ function ColFix()
 	end
 end
 
+function DisCol()
+         for i, v in pairs(game.Workspace:GetDescendants()) do
+		if v:IsA("Part") then
+			v.CanCollide = false
+		end
+	end
+end
+
 function GravFix()
       workspace.Gravity = 196.2
 end
@@ -4707,6 +4732,260 @@ function Gearban()
 	tool:Destroy()
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
         Chat("ungear me")
+end
+
+
+--// ATTACH SHIT \\ --
+
+
+-- // variables \\ --
+local Player = game.Players.LocalPlayer
+local PlayerService = game:GetService("Players")
+
+-- // move object \\ --
+local function moveobject(part, o)
+		if movestatus == false then
+			movestatus = true
+			if o == 1 then
+				if allclear() == true then
+					removelimbs()
+					task.wait()
+					workspace.Gravity = 0
+					Chat("size me .5")
+					workspace.FallenPartsDestroyHeight = 0/0
+					Chat("gear me 108158379")
+					repeat fwait() until Player.Backpack:FindFirstChild("IvoryPeriastron")
+					Player.Character.Humanoid:EquipTool(Player.Backpack:FindFirstChild("IvoryPeriastron"))
+					local ivory = Player.Character:FindFirstChild("IvoryPeriastron")
+					local randcoord = CFrame.new(math.random(-30593, -23388), math.random(-30593, -10455), math.random(-30593, -10455))
+					Player.Character.HumanoidRootPart.CFrame = randcoord
+					PlayerService.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+					if Player.Character.Torso:FindFirstChild("Weld") then
+						Player.Character.Torso:FindFirstChild("Weld"):Destroy()
+					end
+					task.wait(.25)
+					if not Player.Character:FindFirstChild("IvoryPeriastron") then
+						ColFix()
+						GravFix()
+						MRespawn()
+						movestatus = false
+						return
+					end
+					ivory:WaitForChild("Remote"):FireServer(Enum.KeyCode.E)
+					task.wait(.25)
+					attach(part)
+					task.wait(.25)
+					if not Player.Character:FindFirstChild("IvoryPeriastron") then
+						ColFix()
+						GravFix()
+						MRespawn()
+						movestatus = false
+						return
+					end
+					ivory:WaitForChild("Remote"):FireServer(Enum.KeyCode.E)
+					task.wait(.25)
+					movestatus = false
+				else
+						MRespawn()
+						ColFix()
+						GravFix()
+						print("Something went wrong removing the parts.")
+				end
+			elseif o == 2 then
+				local movcord = false
+				if allclear() == true then
+					removelimbs()
+					task.wait()
+					Chat("size me .5")
+					workspace.Gravity = 0
+					workspace.FallenPartsDestroyHeight = 0/0
+					Chat("gear me 108158379")
+					repeat fwait() until Player.Backpack:FindFirstChild("IvoryPeriastron")
+					Player.Character.Humanoid:EquipTool(Player.Backpack:FindFirstChild("IvoryPeriastron"))
+					local ivory = Player.Character:FindFirstChild("IvoryPeriastron")
+					local newcoord = CFrame.new(ogcframes[part.Name][1],ogcframes[part.Name][2],ogcframes[part.Name][3],ogcframes[part.Name][4],ogcframes[part.Name][5],ogcframes[part.Name][6],ogcframes[part.Name][7],ogcframes[part.Name][8],ogcframes[part.Name][9],ogcframes[part.Name][10],ogcframes[part.Name][11],ogcframes[part.Name][12]) * CFrame.new(-1*(part.Size.X/2)-(Player.Character.HumanoidRootPart.Size.X/2),0,0)
+					movcord = true
+					task.spawn(function()
+						while movcord do
+							fwait() 
+							Player.Character.HumanoidRootPart.CFrame = newcoord
+							if movcord == false then 
+								break 
+							end
+						end
+					end)
+					repeat fwait() 
+					until Player.Character.HumanoidRootPart.CFrame == newcoord
+					movcord = false
+					if Player.Character.Torso:FindFirstChild("Weld") then
+						Player.Character.Torso:FindFirstChild("Weld"):Destroy()
+					end
+					PlayerService.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+					task.wait(.25)
+					if not Player.Character:FindFirstChild("IvoryPeriastron") then
+						ColFix()
+						GravFix()
+						MRespawn()
+						movestatus = false
+						return
+					end
+					ivory:WaitForChild("Remote"):FireServer(Enum.KeyCode.E)
+					task.wait(.25)
+					attach(part)
+					task.wait(.25)
+					if not Player.Character:FindFirstChild("IvoryPeriastron") then
+						ColFix()
+						GravFix()
+						MRespawn()
+						movestatus = false
+						return
+					end
+					ivory:WaitForChild("Remote"):FireServer(Enum.KeyCode.E)
+					task.wait(.25)
+					movestatus = false
+				else
+					movcord = false
+					MRespawn()
+					ColFix()
+					GravFix()
+					print("Something went wrong removing the parts.")
+				end
+			elseif o == 3 then
+				if allclear() == true then
+					removelimbs()
+					task.wait()
+					Chat("size me .5")
+					workspace.Gravity = 0
+					workspace.FallenPartsDestroyHeight = 0/0
+					Chat("gear me 108158379")
+					repeat fwait() until Player.Backpack:FindFirstChild("IvoryPeriastron")
+					Player.Character.Humanoid:EquipTool(Player.Backpack:FindFirstChild("IvoryPeriastron"))
+					local ivory = Player.Character:FindFirstChild("IvoryPeriastron")
+					local randcoord = CFrame.new(0, math.random(-14950, -15000), 0)
+					Player.Character.HumanoidRootPart.CFrame = randcoord
+					if Player.Character.Torso:FindFirstChild("Weld") then
+						Player.Character.Torso:FindFirstChild("Weld"):Destroy()
+					end
+					PlayerService.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+					task.wait(.25)
+					if not Player.Character:FindFirstChild("IvoryPeriastron") then
+						ColFix()
+						GravFix()
+						MRespawn()
+						movestatus = false
+						return
+					end
+					ivory:WaitForChild("Remote"):FireServer(Enum.KeyCode.E)
+					task.wait(.05)
+					attach(part)
+					task.wait(.05)
+					if not Player.Character:FindFirstChild("IvoryPeriastron") then
+						ColFix()
+						GravFix()
+						MRespawn()
+						movestatus = false
+						return
+					end
+					ivory:WaitForChild("Remote"):FireServer(Enum.KeyCode.E)
+					task.wait(.25)
+					movestatus = false
+				else
+					MRespawn()
+					ColFix()
+					GravFix()
+					print("Something went wrong removing the parts.")
+				end
+			end
+		end
+	end
+
+-- // remove limbs \\ --
+local function removelimbs()
+	for i,v in pairs(PlayerService.LocalPlayer.Character.Torso:GetChildren()) do
+		if v.Name == "Left Shoulder" then
+				v:Destroy()
+		end
+		if v.Name == "Left Hip" then
+				v:Destroy()
+		end
+		if v.Name == "Right Hip" then
+				v:Destroy()
+		end
+	end
+end
+
+-- // attach 2 part \\ --
+local function attach(part)
+		if Player.Character.Torso:FindFirstChild("Weld") then
+			Player.Character.Torso:FindFirstChild("Weld"):Destroy()
+		end
+		PlayerService.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+		local tasmove = true
+		task.spawn(function()
+			if Player.Character.Torso:FindFirstChild("Weld") then
+				Player.Character.Torso:FindFirstChild("Weld"):Destroy()
+			end
+			while true do
+				fwait()
+				if allclear() == true then
+					PlayerService.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+					Player.Character.HumanoidRootPart.CFrame = part.CFrame * CFrame.new(-1*(part.Size.X/2)-(Player.Character.HumanoidRootPart.Size.X/2),0,0) 
+				elseif allclear() == false then
+					MRespawn()
+					ColFix()
+					GravFix()
+					print("Something went wrong moving the parts. Fixing game and respawning..")
+					tasmove = false
+				end
+				if tasmove == false then 
+					break 
+				end
+			end
+		end)
+		task.spawn(function() 
+			while tasmove do 
+				fwait()
+				act("unpunish me")
+				if allclear() == false then 
+					MRespawn()
+					ColFix()
+					GravFix()
+					print("Something went wrong moving the parts. Fixing game and respawning..")
+					tasmove = false
+				end 
+				if tasmove == false then 
+					break 
+				end
+			end
+		end)
+		
+		repeat fwait() Player.Character.HumanoidRootPart.CFrame = part.CFrame * CFrame.new(-1*(part.Size.X/2)-(Player.Character.HumanoidRootPart.Size.X/2),0,0) 
+		until Player.Character.HumanoidRootPart.CFrame == part.CFrame * CFrame.new(-1*(part.Size.X/2)-(Player.Character.HumanoidRootPart.Size.X/2),0,0)
+		repeat 
+			fwait() 
+			if allclear() == false then
+				break
+			end
+		until 
+		Player.Character.Torso:FindFirstChild("Weld")
+		tasmove = false
+	end
+
+-- // check if ready \\ --
+local function allclear()
+		if workspace:FindFirstChild(game.Players.LocalPlayer.Name) and hasperm == true and workspace:FindFirstChild(game.Players.LocalPlayer.Name) and workspace:FindFirstChild(game.Players.LocalPlayer.Name):FindFirstChild("Humanoid") and workspace:FindFirstChild(game.Players.LocalPlayer.Name):FindFirstChild("Humanoid").Health > 0 then
+			return true
+		elseif workspace:FindFirstChild(game.Players.LocalPlayer.Name) and workspace.Terrain._Game.Admin.Pads:FindFirstChild(game.Players.LocalPlayer.Name.."'s admin") and workspace:FindFirstChild(game.Players.LocalPlayer.Name) and workspace:FindFirstChild(game.Players.LocalPlayer.Name):FindFirstChild("Humanoid") and workspace:FindFirstChild(game.Players.LocalPlayer.Name):FindFirstChild("Humanoid").Health > 0 then
+			return true
+		else
+			movestatus = false
+			return false
+		end
+end
+
+-- // special wait \\ --
+local function fwait()
+		game:GetService("RunService").Heartbeat:Wait()
 end
 
 -- run at end to prevent bugs
