73fd7604506ccd28becc19b2264df6e129f2fdae
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-02-26 14:15:23 +0000

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -24,7 +24,7 @@ end
 _G.executed = true
 
 local prefix = "." -- ANY LENGTH :D
-local blacklist = {"SlenderMan990921","EhiplayYN","e5usp","Asphetto","91txt","LeanConsumer69","xtyzmia","Fixydrqma","Robloxian577226532","jjthejoker7","bbgurrrrl","Alyce_24","cihanahmet1670","Fu14r"} -- slocked users
+local blacklist = {"SlenderMan990921","EhiplayYN","e5usp","Asphetto","91txt","LeanConsumer69","xtyzmia","Fixydrqma","Robloxian577226532","jjthejoker7","Alyce_24","cihanahmet1670","Fu14r"} -- slocked users
 local whitelist = {"me_123eq","me_crashking","ScriptingProgrammer","G_ODt","BANNter_Original","witnessfox22","IceStuds","atprog","dawninja21","Dawninja21alt"} -- not affected by slock
 local newplrslocked = {"place"} -- don't edit!!
 local newplrautoslock = true -- if new players under 21 days join they get blacklisted
