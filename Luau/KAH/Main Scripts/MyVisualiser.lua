7930d2e3ee6d0a39a505851c73bde617212ca2ab
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-12 16:25:10 +0000

    Update and rename My Visualiser.lua to MyVisualiser.lua

--- Luau/KAH/Main Scripts/MyVisualiser.lua
+++ Luau/KAH/Main Scripts/MyVisualiser.lua
@@ -3,7 +3,7 @@
 -- VARIABLES --
 
 -- INPUTS --
-print("--- My Visualizer.lua executed! Created by S_P ---")
+print("--- MyVisualiser.lua executed! Created by S_P ---")
 Chat("h \n\n\n [MyVisualiser.lua]: Executed! v0.00 \n\n\n")
 
 -- FUNCTIONS --
