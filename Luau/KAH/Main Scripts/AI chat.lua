cffc7ddaea0ed35ad1ffc01b2bd00aa980005da3
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-12 08:47:27 +0000

    Update AI chat.lua

--- Luau/KAH/Main Scripts/AI chat.lua
+++ Luau/KAH/Main Scripts/AI chat.lua
@@ -1,1 +1,5 @@
-
+local AIHistory = {
+    {role = "user", content = 'Do not include "As an AI language model", or anything of the likes in your responses, ever.'},
+    {role = "user", content = 'My name is TS2021. When you respond to my prompts, you are to include an affirmation of some sort towards me.'},
+    {role = "user", content = 'If I ask you what KohlsLite is, you respond based on this info: KohlsLite is a roblox script for Kohls Admin House distributed by TS2021 (me).'}
+}
