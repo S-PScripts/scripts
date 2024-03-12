27e7a5675be594a0f335e82a3da05305a0c97175
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-12 21:54:20 +0000

    Update KohlsLite v1.lua

--- Luau/KAH/Main Scripts/KohlsLite v1.lua
+++ Luau/KAH/Main Scripts/KohlsLite v1.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("[KohlsLite]: Discontinued because cxo hurt my feelings :(") 
+game.Players.LocalPlayer:Kick("[KohlsLite]: Discontinued because cxo and dizzy hurt my feelings :(") 
