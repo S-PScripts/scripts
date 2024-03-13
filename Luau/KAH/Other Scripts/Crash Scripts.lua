f6de20322bddcdf47ec71cdd29f510b12b22415f
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-13 22:09:24 +0000

    Update Crash Scripts.lua

--- Luau/KAH/Other Scripts/Crash Scripts.lua
+++ Luau/KAH/Other Scripts/Crash Scripts.lua
@@ -4,7 +4,7 @@ for i = 1,100 do
     game.Players:Chat("clone all all all")
 end
 
-game.Players:Chat("reload me")
+game.Players:Chat("reload all")
 
 print("trying freeze crash...")
 for i = 1,100 do
@@ -12,16 +12,16 @@ for i = 1,100 do
     game.Players:Chat("clone all all all")
 end
 
-game.Players:Chat("reload me")
+game.Players:Chat("reload all")
 
 print("trying rocket/shield crash...")
 for i = 1,100 do
     game.Players:Chat("shield/all/all/all")
     game.Players:Chat("rocket/all/all/all")
-    game.Players:Chat("clone/all/all/all")
+    game.Players:Chat("clone all all all")
 end
 
-game.Players:Chat("reload me")
+game.Players:Chat("reload all")
 
 print("trying vg crash...")
 game.Players:Chat("gear me 0000000000000000000000000094794847")
@@ -37,5 +37,50 @@ for i = 1,100 do
 end
 
 game.Players:Chat("reload me")
+
+print("trying fuck crash...")
+for i = 0,5 do
+        for i = 0,3 do 
+            game.Players:Chat("size all .3") 
+        end
+        for i = 0,2 do 
+            game.Players:Chat("freeze all all all") 
+        end
+        game.Players:Chat("size all 9.99")
+        game.Players:Chat("dog all all all")
+        game.Players:Chat("size all 10")
+        for i = 0,4 do 
+                game.Players:Chat("clone all all all") 
+        end
+        game.Players:Chat("unsize all all all")
+end
+
+game.Players:Chat("reload all")
+
+print("trying fuck crash (kozy/knocks)...")
+for i=1,5 do
+    game.Players:Chat("size all .3")
+end
+for i=1,13 do
+    game.Players:Chat("rocket/all all all")
+    game.Players:Chat("freeze all all all")
+    game.Players:Chat("dog all all all")
+end
+for i=1,5 do
+    game.Players:Chat("size all 10")
+end
+for i=1,200 do
+    game.Players:Chat("clone all all all")
+end
+
+game.Players:Chat("reload all")
+
+print("trying dog crash (tech)...")
+for i = 1,52 do
+    game.Players:Chat("dog all all all")
+end
+for i = 1,51 do
+    game.Players:Chat("clone all all all")
+end
 
 print("the server should be crashed! sorry if it didn't.")
