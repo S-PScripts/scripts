42601576f710324315939c9eef0d5991484f0c80
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-15 08:50:01 +0000

    Update MyVisualiser.lua

--- Luau/KAH/Main Scripts/MyVisualiser.lua
+++ Luau/KAH/Main Scripts/MyVisualiser.lua
@@ -1,6 +1,15 @@
 -- My Visualiser.lua --
 
 -- VARIABLES --
+local function Remind(msg)
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "Visualiser DS",
+		Text = msg,
+		Duration = 1
+	})
+end;
+
+local prefix = "-"
 
 -- INPUTS --
 print("--- MyVisualiser.lua executed! Created by S_P ---")
@@ -10,11 +19,11 @@ Chat("h \n\n\n [MyVisualiser.lua]: Executed! v0.00 \n\n\n")
 
 -- COMMAND HUB --
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
-	    task.wait(0)
-    	if string.sub(msg:lower(), 1, #prefix + 5) == prefix.."kcmds" then
-           CMDPrint()
-	         Remind("Check your console by running /console!")
-      end
+	   task.wait(0)
+    	   if string.sub(msg:lower(), 1, #prefix + 4) == prefix.."cmds" then
+           	CMDPrint()
+	   	Remind("Check your console by running /console!")
+      	   end
 end)
 
 -- COMMAND LIST ---
