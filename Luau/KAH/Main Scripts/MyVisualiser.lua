adc94e36b56519441af2dc5f3f1d1d24124790fa
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-15 08:48:03 +0000

    Update MyVisualiser.lua

--- Luau/KAH/Main Scripts/MyVisualiser.lua
+++ Luau/KAH/Main Scripts/MyVisualiser.lua
@@ -9,3 +9,15 @@ Chat("h \n\n\n [MyVisualiser.lua]: Executed! v0.00 \n\n\n")
 -- FUNCTIONS --
 
 -- COMMAND HUB --
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+	    task.wait(0)
+    	if string.sub(msg:lower(), 1, #prefix + 5) == prefix.."kcmds" then
+           CMDPrint()
+	         Remind("Check your console by running /console!")
+      end
+end)
+
+-- COMMAND LIST ---
+function CMDPrint()
+print("Me_funzysass")
+end
