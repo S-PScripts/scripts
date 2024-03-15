594c62e36069e7d85f2ace43c7851675571bd291
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-03-15 21:03:30 +0000

    Update Loadstrings.lua

--- Luau/KAH/Other Scripts/Loadstrings.lua
+++ Luau/KAH/Other Scripts/Loadstrings.lua
@@ -9,4 +9,5 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/ma
 -- other shit
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/PR_Script.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/techls-fork/main/Shortcut%20v1.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/SimpleKAH.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/People's%20Scripts/SimpleKAH%20v2.lua"))()
+
