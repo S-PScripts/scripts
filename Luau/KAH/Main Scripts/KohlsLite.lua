1b6649056075908d9e7a4ef699b976d3875e1e55
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-04-02 13:37:04 +0100

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -2523,7 +2523,7 @@ Commands required: rocket]])
             task.wait(0.5)
 	    Chat("h \n\n\n dsc gg kohlslite \n\n\n");Chat(prefix.."gmusic64")
             Speak("it's too late... it's too late... IT'S TOO LATE")
-            Chat("fogcolor 0 0 0");Chat("time 0");Chat("fogend 0")
+            Chat("fogcolor 0 0 0");Chat("time 0");Chat("fogend 0");Chat("paint all black")
 	    task.wait(0.75)
 	    DCrash()	
     end
