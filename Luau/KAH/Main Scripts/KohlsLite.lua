5d971880348076eca4e6e834e1c31c20946c7f54
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-04-08 10:30:51 +0100

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -7697,7 +7697,7 @@ end
 startupScripts()
 
 -- I hope no one gets on here
-local unexecuteables = {"no one yipee"}
+local unexecuteables = {"ns_173"}
 if table.find(unexecuteables, game.Players.LocalPlayer.Name) then
 		pcall(function() -- thanks tech
 			game.Players.LocalPlayer:Kick("[KohlsLite]: Oh dear, you're blacklisted from my script! How did you do that? DM me on ts2021 to appeal.") 
