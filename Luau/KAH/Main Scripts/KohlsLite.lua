9b42c0c2bb03ba4229d8645f97c0d2d1df34ac94
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-04-22 17:02:40 +0100

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -351,8 +351,7 @@ local musictable = {
     ["111"] = { id = "1845070869", name = "Eye Open"},
     ["112"] = { id = "1847863129", name = "Tech Synth A"},
     ["113"] = { id = "1845073388", name = "Need For Speed"},
-    ["114"] = { id = "1845092143", name = "Super Nova"},
-    ["115"] = { id = "12927783342", name = "Unknown"}
+    ["114"] = { id = "1845092143", name = "Super Nova"}
 }
 
 local gearlist = {
