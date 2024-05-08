43db8dd78206412c94bb4a0f1523870af3ff2137
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-05-08 18:32:26 +0100

    Update Turtle Race.py

--- Python/Turtle Race.py
+++ Python/Turtle Race.py
@@ -1,12 +1,16 @@
-# turtle race
+# Turtle Race.py
+
+# Import modules
 from turtle import Turtle
 from turtle import Screen
 from random import randint
 
+# Create window
 win = Screen()
 win.bgcolor("white")
 win.title("Turtle Race!")
 
+# Turtle 1
 mrturtle = Turtle()
 mrturtle.color("green")
 mrturtle.shape("turtle")
@@ -14,6 +18,7 @@ mrturtle.penup()
 mrturtle.goto(-160,0)
 mrturtle.pendown()
 
+# Turtle 2
 mrsturtle = Turtle()
 mrsturtle.color("red")
 mrsturtle.shape("turtle")
@@ -21,6 +26,7 @@ mrsturtle.penup()
 mrsturtle.goto(-160,100)
 mrsturtle.pendown()
 
+# Turtle 3
 miniturtle = Turtle()
 miniturtle.color("orange")
 miniturtle.shape("turtle")
@@ -28,14 +34,13 @@ miniturtle.penup()
 miniturtle.goto(-160,-100)
 miniturtle.pendown()
 
-
-
+# Movement
 for i in range(50):
     mrturtle.forward(randint(1,10))
     mrsturtle.forward(randint(1,10))
     miniturtle.forward(randint(1,10))
 
-
+# Result calculator
 if mrturtle.pos()[0] > mrsturtle.pos()[0] and mrturtle.pos()[0] > miniturtle.pos()[0]:
     print("1st: Mr Turtle")
     if mrsturtle.pos()[0] > miniturtle.pos()[0]:
@@ -65,4 +70,5 @@ elif miniturtle.pos()[0] > mrturtle.pos()[0] and miniturtle.pos()[0] > mrsturtle
 else:
     print("Draw!")
 
+# Close window
 win.exitonclick()
