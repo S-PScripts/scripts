16f8d29565b711961175175548923607d6002baf
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-05-10 09:55:17 +0100

    test

---
+++ Luau/Prison Life/PrisonUK.lua
@@ -0,0 +1,7 @@
+-- Prison UK v0
+
+getgenv().pexecuted = true -- don't touch!
+
+getgenv().pprefix = "." -- This can be of any length
+
+getgenv().pversion = "1.57" -- The version of KohlsLite, of course.
