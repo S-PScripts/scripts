0270c3824ba8c33d0dde5ff2f9ecb4ec574cbcd2
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-05-15 09:45:27 +0100

    iy shop

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -7681,6 +7681,31 @@ function SERVERHOP()
         end    
 end
 
+-- alternate serverhop (iy version)
+function AServerhop()
+    if httprequest then
+        local servers = {}
+        local req = httprequest({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", game.PlaceId)})
+        local body = game:GetService("HttpService"):JSONDecode(req.Body)
+
+        if body and body.data then
+            for i, v in next, body.data do
+                if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+                    table.insert(servers, 1, v.id)
+                end
+            end
+        end
+
+        if #servers > 0 then
+            game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], game.Players.LocalPlayer)
+        else
+            return Remind("Sorry, no server could be found.")
+        end
+    else
+        Remind("Sorry, your exploit does not support serverhopping (httprequest)")
+    end
+end
+
 -- keybinds
 game.Players.LocalPlayer:GetMouse().KeyDown:connect(function(key)
         task.wait(0)
