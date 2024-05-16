1642ce8ea0f13e964f27e44af6a953a423cb320e
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2024-05-16 17:53:33 +0100

    Update JSBOMB.js

--- Javascript/JSBOMB.js
+++ Javascript/JSBOMB.js
@@ -1,18 +1,20 @@
-// i don't care what you do with this anymore
+// I don't care what you do with this anymore
 
-// scratch javascript remix bomb made by derv, not me (TS2021)
-// execute the code in console
+// This Scratch Javascript remix bomb was made by RCUTB, not me (TS2021).
+// To use this, execute the code below into your console (Inspect Element - Console section)
 
-var e = 1 // id of starting project
+// Settings
+var e = 1 // This is the project id of the starting project
 
-var h = 1 // next remix number will be this
+var h = 1 // This will be the next project's remix number.
 
-var csrf = "insert here" // scratch X-CSRFToken
+var csrf = "" // your scratch X-CSRFToken (check console on how to get this)
 
-var xtoken = "insert here" // scratch x-token
+var xtoken = "" // your scratch x-token (check console on how to get this)
 
-var cookies = `` // scratch browser cookies
+var cookies = `` // scratch browser cookies (don't edit)
 
+// Code
 jsbomb = async function() {
 
 fetch("https://api.scratch.mit.edu/proxy/projects/"+e+"/share", {
@@ -71,7 +73,7 @@ e = await fetch("https://projects.scratch.mit.edu/?is_remix=1&original_id=" + e 
 
         "referrer": "https://scratch.mit.edu/",
 
-        "body": 'text here', // project's json goes here
+        "body": 'text here', // the project.json will go here. this will be what the project will contain.
 
         "method": "POST",
 
@@ -87,9 +89,9 @@ e = await fetch("https://projects.scratch.mit.edu/?is_remix=1&original_id=" + e 
 
 h=h+1
 
-console.log(h-1); // logs remix number
+console.log(h-1); // this logs the project's remix number to your console
 
-console.log(e) // logs project id
+console.log(e) // this logs the project's id to your console
 
 jsbomb();
 
@@ -98,21 +100,28 @@ jsbomb();
 jsbomb();
 
 
-// delete everything below this line
-// how to get X-CSRFToken
-// comment something
-// go to network and find /session
-// copy the token
+// YOU CAN DELETE EVERYTHING BELOW THIS LINE
 
-// how to get x-token
-// comment something
-// go to scratch.mit.edu/session
-// view the page's source
-// copy the token
+// Instructions
+// How to get your scratch X-CSRFToken
+// 1. Comment something
+// 2. Go to inspect element 
+// 3. Go to the network section
+// 4. Find /session
+// 5. Copy the token
+// Note: This X-CSRFToken will change so you'll need to do this every time you're JSBOMBing.
 
-// how to get project.json
-// download the project you want to use
-// convert it to a zip 
-// open the zip
-// open the folder created by the zip
-// copy the json file in it
+// How to get your scratch x-token
+// 1. Comment something
+// 2. Go to scratch.mit.edu/session
+// 3. View the page's source
+// 4. Copy the x-token
+// Note: This x-token will always stay the same
+
+// How to get the project.json
+// 1. Download the project you want to use
+// 2. Convert it to a zip 
+// 3. Open the zip
+// 4. Open the folder created by the zip
+// 5. Open the json file.
+// 6. Copy the text in it.
