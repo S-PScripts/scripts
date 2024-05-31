19dcfd5d785bfe40d0eee9e64cd48bfd24a73c7c
S-PScripts <ts2021me@outlook.com>
2024-05-31 14:19:21 +0100

    Update KohlsLite NEW.lua

--- Luau/KAH/Main Scripts/KohlsLite NEW.lua
+++ Luau/KAH/Main Scripts/KohlsLite NEW.lua
@@ -76,7 +76,7 @@ local function gamepassCheck(id)
 end
 
 -- Variables for the gamepass checker
-local gamepassData = gamepassCheck(lplr.UserId)
+local gamepassData = gamepassCheck()
 local hasPerm = gamepassData[1]
 local hasPersons = gamepassData[2]
 
@@ -147,38 +147,41 @@ addcredit("You!",
 
 -- Credit list -- 
 addcommand("credits",
-"print out the credits for KohlsLite",
-function()
-        print("Credits:")
-        print("\n\n\n")
-        for i, v in pairs(creditables) do
-            dupe = v.. " - ".. creddesc[v]
-            print(dupe)
-        end
-end)
+	   "print out the credits for KohlsLite",
+	   function()
+        	print("Credits:")
+        	print("\n\n\n")
+        	for i, v in pairs(creditables) do
+            		dupe = v.. " - ".. creddesc[v]
+            		print(dupe)
+        	end
+	    end
+)
 
 -- Command list --
 addcommand("cmds", -- not sure why in scv2 new it doesn't use the system that was created!
-"print out the commands for KohlsLite",
-function()
-        print("Commands:")
-        print("\n\n\n")
-        for i, v in pairs(commands) do
-            dupe = v.. " - ".. descriptions[v]
-            print(dupe)
-        end
-end)
+	   "print out the commands for KohlsLite",
+	   function()
+        	print("Commands:")
+        	print("\n\n\n")
+        	for i, v in pairs(commands) do
+            		dupe = v.. " - ".. descriptions[v]
+            		print(dupe)
+        	end
+	    end
+)
 
 -- Information --
 addcommand("help", 
-"print out information about KohlsLite",
-function()
-      print("Information:")
-      print("\n\n\n")
-      print("You are using KohlsLite by ScriptingProgrammer/ts2021x/S-PScripts. This script has been maintained since 2023.")
-      print("For help, please contact me on Discord at ts2021x.")
-      print("The version you are using is "..admin.klversion2..".")
-end)
+	   "print out information about KohlsLite",
+	    function()
+      		print("Information:")
+      		print("\n\n\n")
+      		print("You are using KohlsLite by ScriptingProgrammer/ts2021x/S-PScripts. This script has been maintained since 2023.")
+      		print("For help, please contact me on Discord at ts2021x.")
+      		print("The version you are using is "..admin.klversion2..".")
+	    end
+)
 
 -- Commands ---
 addcommand(
