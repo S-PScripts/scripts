45c1e4201c2e01efad37085229b96ff6a5ed0d08
S-PScripts <ts2021me@outlook.com>
2024-09-08 12:29:47 +0100

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -7889,48 +7889,52 @@ local char = lplr.Character
 local tor = char:WaitForChild("Torso")
 local oldvel = tor.Velocity
 
-tor.Changed:Connect(function()
-	if tor.Velocity == Vector3.new(0,0,0) then
-		tor.Velocity = oldvel
-	end
-end)
+connections[#connections + 1] =
+    tor.Changed:Connect(function()
+        if tor.Velocity == Vector3.new(0, 0, 0) then
+            tor.Velocity = oldvel
+        end
+    end)
 
-tor.ChildAdded:Connect(function(v)
-	if v:IsA("BodyForce") and antis.antifling then
-		v.Force = Vector3.new(0,0,0)
-		char:FindFirstChildOfClass("Humanoid").Sit = false
-		task.wait()
-		char:FindFirstChildOfClass("Humanoid").Sit = false
-		game:GetService("Debris"):AddItem(v,0)
-		repeat
-			game:GetService("RunService").RenderStepped:Wait()
-		until not v
-	end
-end)
+connections[#connections + 1] =
+    tor.ChildAdded:Connect(function(v)
+        if v:IsA("BodyForce") and antis.antifling then
+            v.Force = Vector3.new(0, 0, 0)
+            char:FindFirstChildOfClass("Humanoid").Sit = false
+            task.wait()
+            char:FindFirstChildOfClass("Humanoid").Sit = false
+            game:GetService("Debris"):AddItem(v, 0)
+            repeat
+                game:GetService("RunService").RenderStepped:Wait()
+            until not v
+        end
+    end)
 
-lplr.CharacterAdded:Connect(function()
-     tor = lplr.Character:WaitForChild("Torso")
-     oldvel = tor.Velocity
+connections[#connections + 1] =
+    lplr.CharacterAdded:Connect(function()
+        tor = lplr.Character:WaitForChild("Torso")
+        oldvel = tor.Velocity
 
-     tor.ChildAdded:Connect(function(v)
-	if v:IsA("BodyForce") and antis.antifling then
-		v.Force = Vector3.new(0, 0, 0)
-		char:FindFirstChildOfClass("Humanoid").Sit = false
-		task.wait()
-		char:FindFirstChildOfClass("Humanoid").Sit = false
-		game:GetService("Debris"):AddItem(v,0)
-		repeat
-			game:GetService("RunService").RenderStepped:Wait()
-		until not v
-	    end
-      end)
+        tor.ChildAdded:Connect(function(v)
+                if v:IsA("BodyForce") and antis.antifling then
+                    v.Force = Vector3.new(0, 0, 0)
+                    char:FindFirstChildOfClass("Humanoid").Sit = false
+                    task.wait()
+                    char:FindFirstChildOfClass("Humanoid").Sit = false
+                    game:GetService("Debris"):AddItem(v, 0)
+                    repeat
+                        game:GetService("RunService").RenderStepped:Wait()
+                    until not v
+                end
+        end)
 
-      tor.Changed:Connect(function()
-	if tor.Velocity == Vector3.new(0,0,0) then
-		tor.Velocity = oldvel
-	    end
-      end)
-end)	
+        tor.Changed:Connect(function()
+                if tor.Velocity == Vector3.new(0, 0, 0) then
+                    tor.Velocity = oldvel
+                end
+        end)
+    end)
+
 
 -- Anti mesh
 connections[#connections + 1] =
