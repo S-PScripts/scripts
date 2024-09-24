de3134c9040cbff303f48555e48e7d1fa519976e
S-PScripts <ts2021me@outlook.com>
2024-09-24 21:21:51 +0100

    poroiwapowi904do

--- Luau/KAH/Other Scripts/Loadstrings.lua
+++ Luau/KAH/Other Scripts/Loadstrings.lua
@@ -1,14 +1,30 @@
 -- CrashX
+
+--[[
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/Crash%20X.lua"))()
 --loadstring(game:HttpGet("https://rentry.co/kohlscrash/raw"))()
+]]
+
+
+
+
 
 -- KohlsLite
-loadstring(game:HttpGet("https://kohlslite.pages.dev"))()
+loadstring(game:HttpGet("https://kohlslite.pages.dev/source.txt"))()
+
+
+
+
+
+-- loadstring(game:HttpGet("https://kohlslite.pages.dev"))()
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/KohlsLite.lua"))()
---loadstring(game:HttpGet("https://rentry.co/kohlslite/raw"))()
+--l oadstring(game:HttpGet("https://rentry.co/kohlslite/raw"))()
 
 -- other shit
+
+--[[
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/Main%20Scripts/PR_Script.txt"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/techls-fork/main/Shortcut%20v1.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/S-PScripts/scripts/main/Luau/KAH/People's%20Scripts/SimpleKAH%20v2.lua"))()
 
+]]
