92f8c9218be6e64656967edd49eff2d29dfcd29f
S-PScripts <ts2021me@outlook.com>
2024-09-30 08:42:27 +0100

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -113,6 +113,8 @@ getgenv().deprefix = "."
 -- The version of KohlsLite
 getgenv().klversion = "1.8"
 
+getgenv().kohlsgui = false -- this exists i guess
+
 -- Chat function
 local function Chat(msg)
       game.Players:Chat(msg)
@@ -14127,6 +14129,143 @@ if purchased_kl or table.find(specialperms, game.Players.LocalPlayer.Name) or ta
 	-- filler
 else
 	Remind("Support my script by buying my gamepass! Just do .purchase.")
+end
+
+-- From Shortcut v2 --
+
+function createKohlsUi(textTable)
+	local kohlsUI = Instance.new("ScreenGui")
+	local TextButton = Instance.new("TextButton")
+	local Frame = Instance.new("Frame")
+	local closeUi = Instance.new("TextButton")
+	local Frame_2 = Instance.new("Frame")
+	local a = Instance.new("TextLabel")
+	local UIGridLayout = Instance.new("UIGridLayout")
+	local clearUi = Instance.new("TextButton")
+
+	--Properties:
+
+	kohlsUI.Name = "kohlsUI"
+	kohlsUI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+	kohlsUI.ResetOnSpawn = false
+
+	TextButton.Parent = kohlsUI
+	TextButton.BackgroundTransparency = 1.000
+	TextButton.Draggable = true
+	TextButton.Position = UDim2.new(0.5, -548, 0.5, -94)
+	TextButton.Size = UDim2.new(0, 385, 0, 20)
+	TextButton.AutoButtonColor = false
+	TextButton.Text = ""
+
+	Frame.Parent = TextButton
+	Frame.ClipsDescendants = true
+	Frame.Size = UDim2.new(0, 400, 0, 400)
+	Frame.ZIndex = 7
+	Frame.Style = Enum.FrameStyle.RobloxRound
+
+	closeUi.Name = "closeUi"
+	closeUi.Parent = Frame
+	closeUi.Position = UDim2.new(1, -15, 0, -5)
+	closeUi.Size = UDim2.new(0, 20, 0, 20)
+	closeUi.ZIndex = 10
+	closeUi.Style = Enum.ButtonStyle.RobloxButtonDefault
+	closeUi.Font = Enum.Font.ArialBold
+	closeUi.Text = "X"
+	closeUi.TextColor3 = Color3.fromRGB(255, 255, 255)
+	closeUi.TextSize = 18.000
+
+	Frame_2.Parent = Frame
+	Frame_2.Position = UDim2.new(0, 0, 0, -9)
+	Frame_2.ZIndex = 8
+
+	a.Name = "a"
+	a.Parent = Frame_2
+	a.BackgroundTransparency = 1.000
+	a.Position = UDim2.new(NAN, 0, NAN, 0)
+	a.Size = UDim2.new(200, 18, 200, 18)
+	a.ZIndex = 8
+	a.Font = Enum.Font.Arial
+	a.Text = ""
+	a.TextColor3 = Color3.fromRGB(255, 255, 255)
+	a.TextSize = 18.000
+	a.TextStrokeTransparency = 0.800
+	a.TextXAlignment = Enum.TextXAlignment.Left
+	a.TextYAlignment = Enum.TextYAlignment.Top
+
+	UIGridLayout.Parent = Frame_2
+	UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+	UIGridLayout.CellSize = UDim2.new(0, 100, 0, 18)
+
+	clearUi.Name = "clearUi"
+	clearUi.Parent = Frame
+	clearUi.Position = UDim2.new(0.944999993, -15, 0, -5)
+	clearUi.Size = UDim2.new(0, 20, 0, 20)
+	clearUi.ZIndex = 10
+	clearUi.Style = Enum.ButtonStyle.RobloxButtonDefault
+	clearUi.Font = Enum.Font.ArialBold
+	clearUi.Text = "C"
+	clearUi.TextColor3 = Color3.fromRGB(255, 255, 255)
+	clearUi.TextSize = 18.000
+
+	-- Scripts:
+
+	local function NNQOKB_fake_script() -- Frame_2.LocalScript 
+		local script = Instance.new('LocalScript', Frame_2)
+
+		function addLabel(txt)
+			local newLb = script.Parent.a:Clone()
+			newLb.Name = "TextLabel"
+			newLb.Text = txt
+			newLb.Parent = script.Parent
+		end
+		
+		function addLabelWCheck(txt)
+			local cnt = 0
+			for i,v in pairs(script.Parent:GetChildren()) do
+				if v.Name == "TextLabel" then
+					cnt = cnt + 1
+				end
+			end
+			if cnt >= 16 then
+				clearLabels()
+			end
+			addLabel(txt)
+		end
+		
+		function clearLabels()
+			for i,v in pairs(script.Parent:GetChildren()) do
+				if v.Name == "TextLabel" then
+					v:Destroy()
+				end
+			end
+		end
+		
+		script.Parent.Parent.closeUi.MouseButton1Click:Connect(function()
+			script.Parent.Parent.Parent.Parent:Destroy()
+		end)
+		
+		script.Parent.Parent.clearUi.MouseButton1Click:Connect(clearLabels)
+		
+		for _, text in ipairs(textTable) do
+			addLabelWCheck(text)
+		end
+	end
+	coroutine.wrap(NNQOKB_fake_script)()
+end
+
+if getgenv().kohlsgui then
+	createKohlsUi(
+                {
+                    "Thanks for using KohlsLite!",
+                    "This script was created by TS2021 (ScriptingProgrammer).",
+		    " ",
+		    "To see the command list:",
+		    "1. Chat .kcmds",
+		    "2. Chat /console",
+		    " ",
+		    "Have fun using my script!"
+	 	}
+	)
 end
 
 --[[
