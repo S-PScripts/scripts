4d9e87bea36627de03c1aba5c5646575a4aae5cb
S-PScripts <ts2021me@outlook.com>
2024-11-27 08:13:53 -0700

    Update KohlsLite.lua

--- Luau/KAH/Main Scripts/KohlsLite.lua
+++ Luau/KAH/Main Scripts/KohlsLite.lua
@@ -1,3 +1,6 @@
+-- Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.
+-- Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.
+
 -- One year since KohlsLite was created!
 -- busy with my life updates might come soon idk
 -- look at this clown: https://github.com/A28er348yg9434/Unknown/blob/main/Unknown
