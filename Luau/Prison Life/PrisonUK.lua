1407f0247cea4157461602fb792de604ae61f7ea
S-PScripts <ts2021me@outlook.com>
2024-12-07 19:20:34 +0000

    if I ever work on this, here's a startup

--- Luau/Prison Life/PrisonUK.lua
+++ Luau/Prison Life/PrisonUK.lua
@@ -1,1 +1,38 @@
--- It will come out someday...
+--[[ 
+ _______   _______   ______   ______    ______   __    __  __    __  __    __ 
+|       \ |       \ |      \ /      \  /      \ |  \  |  \|  \  |  \|  \  /  \
+| $$$$$$$\| $$$$$$$\ \$$$$$$|  $$$$$$\|  $$$$$$\| $$\ | $$| $$  | $$| $$ /  $$
+| $$__/ $$| $$__| $$  | $$  | $$___\$$| $$  | $$| $$$\| $$| $$  | $$| $$/  $$ 
+| $$    $$| $$    $$  | $$   \$$    \ | $$  | $$| $$$$\ $$| $$  | $$| $$  $$  
+| $$$$$$$ | $$$$$$$\  | $$   _\$$$$$$\| $$  | $$| $$\$$ $$| $$  | $$| $$$$$\  
+| $$      | $$  | $$ _| $$_ |  \__| $$| $$__/ $$| $$ \$$$$| $$__/ $$| $$ \$$\ 
+| $$      | $$  | $$|   $$ \ \$$    $$ \$$    $$| $$  \$$$ \$$    $$| $$  \$$\
+ \$$       \$$   \$$ \$$$$$$  \$$$$$$   \$$$$$$  \$$   \$$  \$$$$$$  \$$   \$$
+
+Created by ScriptingProgrammer
+]]
+
+local function Remind(MSG, LENGTH)
+        game.StarterGui:SetCore("SendNotification", {
+                Title = "PrisonUK",
+                Text = MSG,
+                Duration = 1 or LENGTH
+        })
+end;
+
+
+local function Chat(MSG)
+    game.Players:Chat(MSG)
+end
+
+getgenv().prefix = "-"
+
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    
+    if string.sub(msg:lower(), 1, #prefix + 4) == prefix.."test" then
+         Remind("operational")
+    end
+    
+end)
+
+Remind("PrisonUK by S_P")
